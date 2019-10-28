6d14d025125638b24294f017f40fd68c40d880d3
Elisson <rodelison255@gmail.com>
2019-10-28 10:00:54 -0400

    First Commit

---
+++ Asp.Curso/CursoNo.1/CursoNo.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CursoNo._1.MvcApplication" Language="C#" %>
---
+++ Clase/Clase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Eliga Un Numero del 1 al 2 y le dire el color que contiene");
+            Console.Readline(a);
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Seleccionaste El Rojo");
+                    
+                      break;
+                    
+                case 2:
+                    Console.WriteLine("Seleccionaste El Negro");
+                    break;
+                   
+            }Console.ReadKey();
+            
+        }
+    }
+}
